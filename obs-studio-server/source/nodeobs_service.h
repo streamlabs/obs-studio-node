85e710e9cc148b09fd0d3dbebbf6beb007e54517
Richard <rosborne255@yahoo.com>
2025-07-07 16:09:36 -0500

    remove non-functioning Apple VT H264 encoder (#1540)
    * remove non-functioning Apple VT H264 encoder
    
    * disabling this encoder and falling back to the obs_x264 encoder
    
    * run clang-format
    
    * fallback to default software encoder
    
    disable com.apple.videotoolbox.videoencoder.h264 encoder which is not
    functioning properly

--- obs-studio-server/source/nodeobs_service.h
+++ obs-studio-server/source/nodeobs_service.h
@@ -281,4 +281,5 @@ public:
 	static void stopAllOutputs(void);
 	static void setupVodTrack(bool isSimpleMode);
 	static void clearArchiveVodEncoder();
+	static bool isInvalidEncoder(const char *encoderID);
 };
