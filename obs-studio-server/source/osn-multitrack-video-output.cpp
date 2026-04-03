41741862169499ec13a8ab1b16d4b5fdf2d1b499
Richard Osborne <rosborne255@yahoo.com>
2026-04-03 10:45:51 -0500

    multitrack: disable encoder gpu scale
    * disable gpu_scale_type so that scene items are not skipped
    * revert preferred colorspace to VIDEO_CS_709
    * removing the redundant (?) obs_encoder_set_video_mix call. This was
    already set by the create_video_encoder() function

--- obs-studio-server/source/osn-multitrack-video-output.cpp
+++ obs-studio-server/source/osn-multitrack-video-output.cpp
@@ -63,9 +63,18 @@ static void adjust_video_encoder_scaling(const obs_video_info &ovi, obs_encoder_
 	}
 
 	obs_encoder_set_scaled_size(video_encoder, requested_width, requested_height);
+#ifdef __APPLE__
+	// Only set GPU scale type to OBS_SCALE_DISABLE. Defaulting to BICUBIC, etc would
+	// trigger maybe_set_up_gpu_rescale to create an encoder_only_mix with a new pointer
+	// that doesn't match item->canvas, causing scene items to be skipped during scene render.
+	// Instead, leave gpu_scale_type at OBS_SCALE_DISABLE so the encoder connects directly to
+	// the canvas video mix and software (swscale) handles resolution downscaling.
+	obs_encoder_set_gpu_scale_type(video_encoder, OBS_SCALE_DISABLE);
+#else
 	obs_encoder_set_gpu_scale_type(video_encoder, encoder_config.gpu_scale_type.value_or(OBS_SCALE_BICUBIC));
+#endif
 	obs_encoder_set_preferred_video_format(video_encoder, encoder_config.format.value_or(VIDEO_FORMAT_NV12));
-	obs_encoder_set_preferred_color_space(video_encoder, encoder_config.colorspace.value_or(VIDEO_CS_DEFAULT));
+	obs_encoder_set_preferred_color_space(video_encoder, encoder_config.colorspace.value_or(VIDEO_CS_709));
 	obs_encoder_set_preferred_range(video_encoder, encoder_config.range.value_or(VIDEO_RANGE_PARTIAL));
 }
 
@@ -209,8 +218,6 @@ static bool create_video_encoders(const Config &go_live_config, std::shared_ptr<
 
 		if (obs_get_multiple_rendering()) {
 			obs_encoder_set_video_mix(encoder, obs_video_mix_get(ovi, OBS_STREAMING_VIDEO_RENDERING));
-		} else {
-			obs_encoder_set_video_mix(encoder, obs_video_mix_get(ovi, OBS_MAIN_VIDEO_RENDERING));
 		}
 
 		if (!obs_encoder_set_group(encoder, encoder_group.get())) {
