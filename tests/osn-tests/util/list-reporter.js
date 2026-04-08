04caa72cb0a5126a8895c4ee05f14ffb3339f648
Aleksandr Voitenko <avoitenko@logitech.com>
2026-04-08 19:20:35 +0100

    Updated tests reporter to report retries

--- tests/osn-tests/util/list-reporter.js
+++ tests/osn-tests/util/list-reporter.js
@@ -6,19 +6,69 @@ function ListReporter(runner) {
     mocha.reporters.Base.call(this, runner);
     var passes = 0;
     var failures = 0;
+    var retries = 0;
     var failedTestCases = [];
     var flakyTestCases = [];
     var flakyReportPath = process.env.OSN_FLAKY_TESTS_FILE || 'flaky-tests.json';
     var testLine = "";
 
+    function getRetryCount(test) {
+        if (typeof test.currentRetry === 'function') {
+            return test.currentRetry();
+        }
+
+        return test.currentRetry || 0;
+    }
+
+    function getMaxRetries(test) {
+        if (typeof test.retries === 'function') {
+            return test.retries();
+        }
+
+        return test._retries || 0;
+    }
+
+    function getTestLineFromError(err) {
+        if (!err || !err.stack) {
+            return "";
+        }
+
+        var regex = /(?<=src\\)(.*?)(?=\n)/;
+        var str = err.stack.match(regex);
+        if (str != null) {
+            return str[0].substr(0, str[0].lastIndexOf(":"));
+        }
+
+        return "";
+    }
+
     runner.on('start', function() {
         console.log('');
+    });
+
+    runner.on('retry', function(test, err) {
+        retries++;
+
+        var currentRetry = getRetryCount(test);
+        var maxRetries = getMaxRetries(test);
+        var retryTestLine = getTestLineFromError(err);
+        var retryDetails = retryTestLine ? ' | [FAIL] ' + retryTestLine : '';
+
+        console.log(
+            '%s: [TEST CASE] %s [RETRY %d/%d]%s',
+            test.parent.title,
+            test.title,
+            currentRetry,
+            maxRetries,
+            retryDetails
+        );
+        console.log('%s: [RETRY REPORT] Error - %s', test.parent.title, err.message);
     });
 
     runner.on('pass', function(test) {
         passes++;
 
-        if (typeof test.currentRetry === 'function' && test.currentRetry() > 0) {
+        if (getRetryCount(test) > 0) {
             flakyTestCases.push({
                 suite: test.parent.title,
                 title: test.title,
@@ -27,7 +77,7 @@ function ListReporter(runner) {
                     : test.parent.title + ' ' + test.title,
                 file: test.file || '',
                 duration: test.duration,
-                attempts: test.currentRetry() + 1
+                attempts: getRetryCount(test) + 1
             });
         }
 
@@ -38,11 +88,7 @@ function ListReporter(runner) {
         failures++;
 
         // Getting test line with the expect check that failed
-        var regex = /(?<=src\\)(.*?)(?=\n)/;
-        var str = err.stack.match(regex);
-        if (str != null) {
-            testLine = str[0].substr(0, str[0].lastIndexOf(":"));
-        }
+        testLine = getTestLineFromError(err);
 
         // Formatting failure message
         var testCaseFailMsg = test.parent.title + ': [TEST CASE] ' + test.title + ' | [FAIL] ' + testLine;
@@ -56,6 +102,10 @@ function ListReporter(runner) {
     runner.on('end', function() {
         console.log('%d passing', passes);
 
+        if (retries >= 1) {
+            console.log('%d retried', retries);
+        }
+
         if (failures >= 1) {
             console.log('%d failing', failures);
 
@@ -63,6 +113,21 @@ function ListReporter(runner) {
                 console.log('  - %s', testCase);
             });
             
+            console.log('');
+        }
+
+        if (flakyTestCases.length >= 1) {
+            console.log('%d flaky', flakyTestCases.length);
+
+            flakyTestCases.forEach(function(testCase) {
+                console.log(
+                    '  - %s: [TEST CASE] %s [FLAKY PASS] %d attempt(s)',
+                    testCase.suite,
+                    testCase.title,
+                    testCase.attempts
+                );
+            });
+
             console.log('');
         }
 
