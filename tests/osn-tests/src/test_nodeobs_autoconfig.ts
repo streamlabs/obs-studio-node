168cab5720eae9f2dd699de6d469a0d32e80b226
Richard Osborne <rosborne255@yahoo.com>
2026-04-22 18:28:00 -0500

    test_nodeobs_autoconfig: use global mocha timeout
    * remove timeout I added in commit 25eaa10. Use global timeout

--- tests/osn-tests/src/test_nodeobs_autoconfig.ts
+++ tests/osn-tests/src/test_nodeobs_autoconfig.ts
@@ -9,7 +9,6 @@ import { deleteConfigFiles } from '../util/general';
 const testName = 'nodeobs_autoconfig';
 
 describe(testName, function() {
-    this.timeout(30000)
     let obs: OBSHandler;
     let hasTestFailed: boolean = false;
 
