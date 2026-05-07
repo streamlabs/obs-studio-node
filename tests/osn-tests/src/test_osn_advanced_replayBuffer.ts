6ffa9fdebd349cf385fd2daf7dbbb9792f539677
Richard Osborne <rosborne255@yahoo.com>
2026-05-07 17:07:56 -0500

    Revert "osn-replay-buffer: invoke output handler to save replays (#1689)"
    This reverts commit e0d4c056162aa41190dff256a5afca00bbb02a41.
    
    This implementation overwrites the file (release blocker)

--- tests/osn-tests/src/test_osn_advanced_replayBuffer.ts
+++ tests/osn-tests/src/test_osn_advanced_replayBuffer.ts
@@ -131,6 +131,9 @@ describe(testName, () => {
     });
 
     it('Start advanced replay buffer - Use Recording', async function() {
+        if (obs.isDarwin()) {
+            this.skip();
+        }
         replayBuffer = osn.AdvancedReplayBufferFactory.create();
         replayBuffer.path = path.join(path.normalize(__dirname), '..', 'osnData');
         replayBuffer.format = osn.ERecordingFormat.MP4;
@@ -258,6 +261,9 @@ describe(testName, () => {
     });
 
     it('Start advanced replay buffer - Use Stream through Recording', async function() {
+        if (obs.isDarwin()) {
+            this.skip();
+        }
         replayBuffer = osn.AdvancedReplayBufferFactory.create();
         replayBuffer.path = path.join(path.normalize(__dirname), '..', 'osnData');
         replayBuffer.format = osn.ERecordingFormat.MP4;
